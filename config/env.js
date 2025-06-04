c452aacebcfa77a66a302adf290844b38bccb736
msaakaash <msaakaash@hotmail.com>
2025-06-04 21:29:51 +0530

    Added upstash config

--- config/env.js
+++ config/env.js
@@ -4,4 +4,8 @@ import { config } from 'dotenv';
 config({path:`.env.${process.env.NODE_ENV || 'development'}.local`});
 
 // eslint-disable-next-line no-undef
-export const { PORT,NODE_ENV,DB_URI,JWT_SECRET,JWT_EXPIRES_IN,ARCJET_ENV,ARCJET_KEY} = process.env;
+export const {
+     PORT,NODE_ENV,DB_URI,JWT_SECRET,
+     JWT_EXPIRES_IN,ARCJET_ENV,ARCJET_KEY,
+     // eslint-disable-next-line no-undef
+     QSTASH_TOKEN,QSTASH_URL} = process.env;
