398b1b843ab3e6e6d7deddbe29d69b08f7ae3f3a
msaakaash <msaakaash@hotmail.com>
2025-06-05 22:53:53 +0530

    Added workflow route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 import swaggerDocs from './docs/swagger.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 const app = express();
 // Add this line to parse JSON request bodies
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/subscriptions',subscriptionRouter);
+app.use('/api/v1/workflows',workflowRouter); // Assuming you have a workflow router
 app.use(errorMiddleware);
 app.use(arcjetMiddleware)
 
